4500d520eb2d8a1b59e34798a512a1481452b441
Maykonn <maykonn@redfuel.com.br>
2017-03-16 15:12:59 -0300

    Test

--- src/index.php
+++ src/index.php
@@ -1,14 +1,13 @@
 <?php
-#echo phpinfo();
+#try {
+#    $dbh = new PDO('mysql:host=db;dbname=myapp', 'admin', 'password');
+#
+#    foreach($dbh->query('SELECT * from mytable') as $row) {
+#        print_r($row);
+#    }
+#    $dbh = null;
+#} catch (Exception $e) {
+#    print_r($e->getMessage());
+#}
 
-try {
-    $dbh = new PDO('mysql:host=db;dbname=myapp', 'admin', 'password');
-
-    foreach($dbh->query('SELECT * from mytable') as $row) {
-        print_r($row);
-    }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
+echo phpinfo();
